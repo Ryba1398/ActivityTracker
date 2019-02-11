0dff980e0060bd90214ae186bf526e9d036161fb
Nikolay Rybin <rubs1998@gmail.com>
2019-02-11 13:48:48 +0300

    Initial Commit

---
+++ ActivityTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityTracker
+//
+//  Created by Nikolay on 11/02/2019.
+//  Copyright © 2019 Nikolay Rybin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
